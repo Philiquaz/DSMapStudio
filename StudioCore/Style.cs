0666e62c141a3b5b4d57c338a8e35ca13da57be7
Philiquaz <Philiquaz@gmail.com>
2023-03-20 22:49:35 +0000

    colourise

--- StudioCore/Style.cs
+++ StudioCore/Style.cs
@@ -8,17 +8,90 @@ namespace StudioCore
     public class Style
     {
         public static Style Dark { get; private set; } = new();
-        //public static Style Light { get; private set; } = new();
-        public static Style Current { get; private set; } = Dark;
+        public static Style Light { get; private set; } = new()
+        {
+            // General settings
+            PRIMARY_TEXT_COLOUR = new (0f, 0f, 0f, 1f),
+            PRIMARY_BG_COLOUR = new (0.988f, 0.988f, 0.988f, 1.0f),
+            SECONDARY_BG_COLOUR = new (0.988f, 0.988f, 0.988f, 1.0f),
+            HIGHLIGHT_BG_COLOUR = new (0.945f, 0.961f, 0.984f, 1.0f),
+            BORDER_COLOUR = new (0.839f, 0.898f, 0.961f, 1.0f),
+            FRAME_BG_COLOUR = new (0.941f, 0.941f, 0.941f, 1.0f),
+            ELEMENT_HOVERED_COLOUR = new (0.839f, 0.898f, 0.961f, 1.0f),
+            ELEMENT_ACTIVE_COLOUR = new (0.200f, 0.600f, 1.000f, 1.0f), //?
+            SCROLLBAR_BG_COLOUR = new (0.929f, 0.929f, 0.929f, 1.0f),
+            SCROLLBAR_GRAB_COLOUR = new (0.863f, 0.863f, 0.961f, 1.0f),
+            SLIDE_GRAB_COLOUR = new (0.635f, 0.635f, 0.635f, 1.0f),
+            SLIDE_ACTIVE_COLOUR = new (1.000f, 1.000f, 1.000f, 1.0f),
+            HEADER_COLOUR = new (0.000f, 0.478f, 0.800f, 1.0f),
+            HEADER_ACTIVE_COLOUR = new (0.161f, 0.550f, 0.939f, 1.0f),
+            TAB_COLOUR = new (0.988f, 0.988f, 0.988f, 1.0f),            //?
+            TAB_HOVERED_COLOUR = new (0.110f, 0.592f, 0.918f, 1.0f),
+            CHECKMARK_COLOUR = new (0.000f, 0.000f, 0.000f, 1.0f),
+            // Settings window
+            SETTINGS_BG_COLOUR = new (0.9f, 0.9f, 0.9f, 0.98f),
+            SETTINGS_TITLE_COLOUR = new (0.75f, 0.75f, 0.75f, 1.0f),
+            SETTINGS_HEADER_COLOUR = new (0.75f, 0.75f, 0.8f, 0.4f),
+            // MSB Editor misc
+            MSB_BG_COLOUR = new Vector4(0.824f, 0.824f, 0.827f, 1.0f),
+            MSB_CHALICE_ERROR_BG_COLOUR = new (0.8f, 0.2f, 0.2f, 1.0f),
+            // DisplayGroup window
+            DRAW_DISP_BG_COLOUR = new (0.4f, 0.4f, 0.06f, 1.0f),
+            DRAW_DISP_CHECK_COLOUR = new (1f, 1f, 0.02f, 1.0f),
+            DRAW_BG_COLOUR = new (0.02f, 0.3f, 0.02f, 1.0f),
+            DRAW_CHECK_COLOUR = new (0.2f, 1.0f, 0.2f, 1.0f),
+            DISP_BG_COLOUR = new (0.4f, 0.06f, 0.06f, 1.0f),
+            DISP_CHECK_COLOUR = new (1.0f, 0.2f, 0.2f, 1.0f),
+            // MSB multi-edit
+            MSB_PROPEDIT_MULTIOBJECT_COLOUR = new (0.5f, 1.0f, 0.0f, 1.0f),
+            MSB_PROPEDIT_MULTIOBJECT_BG_COLOUR = new (0.0f, 0.5f, 0.0f, 0.1f),
+            // Param PropEditor
+            PARAM_MODIFIED_BG_COLOUR = new (0.2f, 0.22f, 0.2f, 1.0f),
+            PARAM_CONFLICT_BG_COLOUR = new (0.25f, 0.2f, 0.2f, 1.0f),
+            PARAM_MERGEABLE_BG_COLOUR = new (0.2f, 0.2f, 0.236f, 1.0f),
+            REFERENCE_ANY_DATA_COLOUR = new (0.5f, 0.2f, 0.5f, 1.0f),
+            PARAM_DEFAULT_DATA_COLOUR = new (0.25f, 0.25f, 0.25f, 1.0f),
+            PARAM_AUX_BG_COLOUR = new (0.824f, 0.824f, 0.827f, 1.0f),
+            PARAM_AUX_COLOUR = new (0.4f, 0.4f, 0.4f, 1.0f),
+            // Text general colouration
+            UPDATE_COLOUR = new (0.0f, 0.5f, 0.0f, 1.0f),
+            WARNING_COLOUR = new (0.5f, 0f, 0f, 1.0f),
+            WARNING_HINT_COLOUR = new (0.0f, 0.0f, 0.0f, 1.0f),
+            HINT_ICON_COLOUR = new (0.25f, 0.25f, 0.5f, 1.0f),
+            MSB_DUPLICATION_HINT_COLOUR = new (0.0f, 0.0f, 0.0f, 0.5f),
+            MSB_EDITOR_PARAM_HEADER_COLOUR = new (0.2f, 0.2f, 0.2f, 1.0f),
+            PARAM_EDITOR_PARAM_HEADER_COLOUR = new (0.2f, 0.2f, 0.3f, 1.0f),
+            PARAM_HOTRELOAD_DISCLAIM_COLOUR = new (0.4f, 0.4f, 0.0f, 1.0f),
+            PARAM_UPGRADE_COLOUR = new (0.0f, 0.4f, 0f, 1.0f),
+            PARAM_TRUE_NAME_COLOUR = new (0.5f, 0.3f, 0.15f, 1.0f),
+            PARAM_FMG_COLOUR = new (0.4f, 0.4f, 0.0f, 1.0f),
+            TEXT_WARNING_COLOUR = new (0.5f, 0.0f, 0.0f, 1.0f),
+            // Text type colouration,
+            REFERENCE_PARAM_LABEL_COLOUR = new (0.4f, 0.4f, 0.0f, 1.0f),
+            REFERENCE_PARAM_LABEL_DISABLED_COLOUR = new (0.3f, 0.3f, 0.3f, 1.0f),
+            REFERENCE_FMG_LABEL_COLOUR = new (0.4f, 0.4f, 0.0f, 1.0f),
+            REFERENCE_ENUM_LABEL_COLOUR = new (0.4f, 0.4f, 0.0f, 1.0f),
+            REFERENCE_PARAM_VALUE_COLOUR = new (0.5f, 0.2f, 0.2f, 1.0f),
+            REFERENCE_PARAM_VALUE_INVALID_COLOUR = new (0.8f, 0.8f, 0.8f, 1.0f),
+            REFERENCE_FMG_VALUE_COLOUR = new (0.5f, 0.2f, 0.2f, 1.0f),
+            REFERENCE_ENUM_VALUE_COLOUR = new (0.5f, 0.2f, 0.2f, 1.0f),
+            // Text state colouration,
+            MSB_ITEM_VISIBLE_COLOUR = new Vector4(0.0f, 0.0f, 0.0f, 1.0f),
+            MSB_ITEM_INVISIBLE_COLOUR = new Vector4(0.5f, 0.5f, 0.5f, 1.0f),
+            MSB_MAP_ALIAS_COLOUR = new (0.4f, 0.4f, 0.0f, 1.0f),
+            MSB_MAP_ALIAS_UNUSED_COLOUR = new (0.5f, 0.0f, 0.0f, 1.0f),
+            PARAM_PRIMARY_MODIFIED_COLOUR = new Vector4(0.3f, 0.5f, 0.3f, 1.0f),
+            PARAM_PRIMARY_UNMODIFIED_COLOUR = new Vector4(0.1f, 0.1f, 0.1f, 1),
+            PARAM_AUX_ADDED_COLOUR = new Vector4(0.3f, 0.3f, 0.5f, 1.0f),
+            PARAM_AUX_CONFLICT_COLOUR = new Vector4(0.5f, 0.3f, 0.3f, 1.0f)
+        };
+        public static Style Current { get; private set; } = Light;
 
         // General settings
 
         public Vector4 REAL_BG_COLOUR = new (0.0f, 0.0f, 0.0f, 0.0f);
 
-        public Vector4 SETTINGS_BG_COLOUR = new (0f, 0f, 0f, 0.98f);
-        public Vector4 SETTINGS_TITLE_COLOUR = new (0.25f, 0.25f, 0.25f, 1.0f);
-        public Vector4 SETTINGS_HEADER_COLOUR = new (0.3f, 0.3f, 0.6f, 0.4f);
-
+        public Vector4 PRIMARY_TEXT_COLOUR = new (1.0f, 1.0f, 1.0f, 1.0f);
         public Vector4 PRIMARY_BG_COLOUR = new (0.176f, 0.176f, 0.188f, 1.0f);
         public Vector4 SECONDARY_BG_COLOUR = new (0.176f, 0.176f, 0.188f, 1.0f);
         public Vector4 HIGHLIGHT_BG_COLOUR = new (0.106f, 0.106f, 0.110f, 1.0f);
@@ -35,6 +108,11 @@ namespace StudioCore
         public Vector4 TAB_COLOUR = new (0.176f, 0.176f, 0.188f, 1.0f);
         public Vector4 TAB_HOVERED_COLOUR = new (0.110f, 0.592f, 0.918f, 1.0f);
         public Vector4 CHECKMARK_COLOUR = new (1.000f, 1.000f, 1.000f, 1.0f);
+
+        // Settings window
+        public Vector4 SETTINGS_BG_COLOUR = new (0f, 0f, 0f, 0.98f);
+        public Vector4 SETTINGS_TITLE_COLOUR = new (0.25f, 0.25f, 0.25f, 1.0f);
+        public Vector4 SETTINGS_HEADER_COLOUR = new (0.3f, 0.3f, 0.6f, 0.4f);
 
         // MSB Editor misc
         public Vector4 MSB_BG_COLOUR = new Vector4(0.145f, 0.145f, 0.149f, 1.0f);
